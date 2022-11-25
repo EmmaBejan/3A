c2b68f6e0bd05a4decc7d9ebeace585113a62e4c
E <113189521+EmmaBejan@users.noreply.github.com>
2022-11-25 13:08:33 +0000

    Math

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ math.pi
 
 Large = 6.00
 ExtraLarge = 10.00
-
 OneTop = 1.00
 TwoTop = 1.75
 ThreeTop = 2.50
 FourTop = 3.35
+print ("Hello good sir, what size pizza would you like?")
 
+if Large > ExtraLarge:
+  print ("Ok that will be 6$. Would you like any toppings?")
