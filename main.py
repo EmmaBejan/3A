234e9e6b987d79df4c41307471c8a6a7ea0de426
E <113189521+EmmaBejan@users.noreply.github.com>
2022-11-29 15:18:36 +0000

    %

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 math.pi
 
+HST = 13%
 Large = 6.00
 ExtraLarge = 10.00
 OneTop = 1.00
@@ -11,3 +12,7 @@ print ("Hello good sir, what size pizza would you like?")
 
 if Large > ExtraLarge:
   print ("Ok that will be 6$. Would you like any toppings?")
+if ExtraLarge > Large:
+  print ("Ok that will be 10$. Would you like any toppings?")
+
+
